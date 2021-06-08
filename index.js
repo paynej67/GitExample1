af47f4b07574ce7bced012169fe6b2e98b32a86a
paynej67 <jeffreyjpayne1@outlook.com>
2021-06-08 15:17:40 -0500

    added context to index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+myObj =    [
+    {
+        "Id" : "1",
+        "FirstName" : "Benjamin" ,
+        "LastName" : "Finkel" ,
+        "Email" : "ben.finkel@cbtnuggets.com"
+    },
+    {
+        "Id" : "2",
+        "FirstName" : "Jane" ,
+        "LastName" : "Doe" ,
+        "Email" : "jane.doe@cbtnuggets.com"
+    },
+    {
+        "Id" : "3",
+        "FirstName" : "Pat" ,
+        "LastName" : "Smith" ,
+        "Email" : "pat.smith@cbtnuggets.com"
+    }
+]
+
+console.log(myOBj[1].name[1]);
